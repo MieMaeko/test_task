0c3eaca1ab01d0db9649dddb62ab8a7c19ff4f45
miemaeko <mie.2015@mail.ru>
2026-01-22 20:44:06 +0300

    addead api, media

---
+++ progress.js
@@ -0,0 +1,37 @@
+function progressApi(rootElement) {
+    const bar = rootElement.querySelector('.progress-bar');
+    return {
+        setProgress(value) {
+            bar.style.setProperty('--pg-bar', `${value}%`)
+        },
+        Hide() {
+            bar.classList.toggle('hidden');
+        },
+        Animate() {
+            bar.classList.toggle('animated');
+        }
+    }
+}
+
+window.onload = () => {
+    const root = document.querySelector('.progress-block');
+    const api = progressApi(root);
+    const inp = document.getElementById('progress-value');
+    const hide = document.getElementById('progress-hide');
+    const anim = document.getElementById('progress-animate')
+
+    inp.addEventListener("input", () => {
+        inp.value = inp.value.replace(/[^\d]/g, "");
+        if (inp.value > 100) inp.value = 100;
+        if (inp.value < 0) inp.value = 0;
+        api.setProgress(inp.value)
+    })
+
+    hide.addEventListener('change', () => {
+        api.Hide()
+    })
+
+    anim.addEventListener('change', () => {
+        api.Animate()
+    })
+}
